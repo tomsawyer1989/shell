479992b63ec10aaa8556cf15597e2102936110a0
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2024-07-10 22:32:12 -0500

    feat(configuration): Module Federation implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from "@angular-architects/module-federation";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        loadChildren: () =>
+            loadRemoteModule({
+                type: "module",
+                remoteEntry: "http://localhost:4201/remoteEntry.js",
+                exposedModule: "./mf1Routes"
+            }).then(m => m.routes)
+    }
+];
